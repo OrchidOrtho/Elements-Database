176c4f2a7115cf03bcaadaab6414861f5b11ad70
Jeff Phillips <43249255+jrpOrchid@users.noreply.github.com>
2018-09-25 11:42:09 -0400

    Initial Commit

---
+++ Elements Database/Live/Scripts/Update BasePerson Email address from Audit Trail.sql
@@ -0,0 +1,6 @@
+﻿/* NOTE: This script requires the _jrpBasePersonAuditTrailEmailAddress view in the Views folder. */
+
+UPDATE       __BasePerson
+SET                Email = _jrpBasePersonAuditTrailEmailAddress.OldValue
+FROM            __BasePerson INNER JOIN
+                         _jrpBasePersonAuditTrailEmailAddress ON __BasePerson.Id = _jrpBasePersonAuditTrailEmailAddress.ItemId
